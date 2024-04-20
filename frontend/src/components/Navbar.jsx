5daf726807e013a3e5ef4d3e657e2a6c5bd3d9f8
SubhamayDebnath <subhamaydebnath6@gmail.com>
2024-04-20 23:57:25 +0530

    #

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import { IoClose, IoMenu } from "react-icons/io5";
+function Navbar() {
+  const [menu,setMenu]=useState(false);
+  return (
+    <header>
+      <nav className='container flex items-center justify-between py-4 px-4 sm:px-6 lg:px-8'>
+        <Link to={'/'} className='text-2xl font-extrabold text-black'>DOTNOTE</Link>
+        <button className='h-9 w-9 md:hidden flex items-center justify-center shadow-sm rounded-full transition-colors ease-in-out text-black border border-gray-200 hover:bg-gray-50' onClick={()=>setMenu(!menu)}>
+          {menu ? <IoClose size={24} />: <IoMenu size={24} />}
+        </button>
+        <div className={`menu ${menu ? "active":""}`}>
+          <div className="flex md:flex-row flex-col item-center">
+            <Link to={"/"} className='nav_link'>Home</Link>
+            <Link to={"/notes"} className='nav_link'>Notes</Link>
+            <Link to={"/about"} className='nav_link'>About us</Link>
+            <Link to={"/contact"} className='nav_link'>Contact us</Link>
+            <Link to={"/"} className='nav_btn'>Get Started</Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
